c31a4700345abd03190d067633e24045c95b4c96
Mahmoud <mmatouk@users.noreply.github.com>
2025-01-30 16:14:23 +0000

    Adding resources for the sample app

---
+++ resources/3_3/App.js
@@ -0,0 +1,77 @@
+import './App.css';
+import React, { useState } from 'react';
+// App.js
+
+import { useAuth } from "react-oidc-context";
+
+function App() {
+  const [htmlContent, setHtmlContent] = useState('');
+  const updateHtml = (newContent) => {
+    setHtmlContent('<pre>' + newContent + '</pre>');
+  };
+
+  const auth = useAuth();
+
+  /**
+   * signOutRedirect function called from signOut button and it redirects the user
+   * to application's landing page after calling Cognito logout endpoint to end session
+  **/
+  const signOutRedirect = () => {
+    auth.removeUser();
+
+    const clientId = "4gm4m86r2s175hqqsvp8es1fgc";
+    const logoutUri = "https://fluffy-tribble-6vggp6xvx4r35vrg-3000.app.github.dev/";
+    const cognitoDomain = "https://us-east-1cijtqmvhq.auth.us-east-1.amazoncognito.com";
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+  };
+
+  /**
+   * registerPassKeyRedirect is an authenticated function that allows end-user tp
+   * register PassKey by redirecting to Cognito passkeys/add endpoint
+   */
+  const registerPasskeyRedirect = () => {
+    const clientId = "4gm4m86r2s175hqqsvp8es1fgc";
+    const redirectUri = "https://fluffy-tribble-6vggp6xvx4r35vrg-3000.app.github.dev/";
+    const cognitoDomain = "https://us-east-1cijtqmvhq.auth.us-east-1.amazoncognito.com";
+    window.location.href = `${cognitoDomain}/passkeys/add?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}`;
+  };
+
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Encountering error... {auth.error.message}</div>;
+  }
+
+  if (auth.isAuthenticated) {
+    return (
+      <div>
+        <div style={{ height: '200px' }}>
+          <pre> Hello: {auth.user?.profile.email} </pre>
+          <pre> ID Token: {auth.user?.id_token} </pre>
+          <pre> Access Token: {auth.user?.access_token} </pre>
+          <pre> Refresh Token: {auth.user?.refresh_token} </pre>
+
+          <button onClick={() => signOutRedirect()}>Sign out</button>
+          <button onClick={() => registerPasskeyRedirect()}>Register Passkey</button>
+        </div>
+        Response results:
+        <div style={{ backgroundColor: 'lightgrey' }} dangerouslySetInnerHTML={{ __html: htmlContent }} />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <div style={{ height: '200px' }}>
+          <button onClick={() => auth.signinRedirect()}>Sign in</button>
+        </div>
+        Response results:
+        <div style={{ backgroundColor: 'lightgrey' }} dangerouslySetInnerHTML={{ __html: htmlContent }} />
+      </div>
+    );
+  }
+
+}
+
+export default App;
